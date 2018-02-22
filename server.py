b2033a3b6222b9eb7b1b6126c73ea6e51d22e719
Brett Crowell <brettcrowell@gmail.com>
2018-02-21 21:31:48 -0500

    switch to post

--- server.py
+++ server.py
@@ -3,8 +3,8 @@
 
 from sys import argv
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import urlparse, parse_qs
 from example_commands import ExampleCommands
+import json
 
 class S(BaseHTTPRequestHandler):
 
@@ -20,22 +20,24 @@ class S(BaseHTTPRequestHandler):
     def do_GET(self):
         self._set_headers()
         self.wfile.write("got".encode("utf-8"))
-        query_params = parse_qs(urlparse(self.path).query)
-        self._set_divoom()
-
-        action = query_params["action"][0]
-        value = query_params["value"][0]
-
-        with self.commands as commands:
-            if (action == "show_file"):
-                commands.show_file(value)
-            elif (action == "draw_text"):
-                commands.draw_text(value)
 
     def do_POST(self):
-        # Doesn't do anything with posted data
         self._set_headers()
         self.wfile.write("posted".encode("utf-8"))
+        self._set_divoom()
+
+        length = int(self.headers["content-length"])
+        payload = self.rfile.read(length)
+        data = json.loads(payload)
+
+        action = data["action"]
+        value = data["value"]
+
+        with self.commands as commands:
+            if action == "show_file":
+                commands.show_file(value)
+            elif action == "draw_text":
+                commands.draw_text(value)
 
     def do_HEAD(self):
         self._set_headers()
