473999eabeeb4acae31df82e17da8890a5fd5b89
Brett Crowell <brettcrowell@gmail.com>
2018-02-21 20:48:08 -0500

    update and add tests for pixel_array

--- divoom_pixel_array_test.py
+++ divoom_pixel_array_test.py
@@ -1,19 +1,23 @@
 import unittest
-import divoom_pixel_array
+from divoom_pixel_array import pixel_array_to_divoom, divoom_to_pixel_array
 
 class TestDivoomAuraBoxPixelArray(unittest.TestCase):
 
-    TEST_BITS = [0, 0, 0, 0, 0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, 12, 12, 0,
-                 0, 0, 0, 0, 0, 0, 0, 12, 12, 0, 0, 0, 0, 0, 0, 0, 12, 12, 12,
-                 0, 0, 0, 0, 0, 0, 12, 12, 12, 12, 0, 0, 0, 0, 0, 12, 12, 12,
-                 12, 12, 0, 0, 0, 0, 0, 0, 12, 12, 12, 12, 0, 0, 0, 0, 12, 0, 0,
-                 12, 12, 12, 0, 0, 0, 12, 12, 0, 0, 12, 12, 12, 0, 0, 12, 12,
-                 12, 0, 0, 0, 12, 12, 0]
+    PIXEL_ARRAY = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0,
+                   0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
+                   0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1,
+                   1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0,
+                   1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1,
+                   1, 0, 0, 0, 1, 1, 0]
 
-    TEST_DIVOOM = [0, 0, 0, 0, 4, 0, 0, 0, 64, 4, 0, 0, 0, 64, 4, 0, 0, 0, 68,
-                   4, 0, 0, 64, 68, 4, 0, 0, 68, 68, 4, 0, 0, 64, 68, 4, 0, 64,
-                   0, 68, 4, 0, 68, 0, 68, 4, 64, 68, 0, 64, 4]
+    DIVOOM_ENCODED = [0, 0, 0, 0, 1, 0, 0, 0, 16, 1, 0, 0, 0, 16, 1, 0, 0, 0,
+                      17, 1, 0, 0, 16, 17, 1, 0, 0, 17, 17, 1, 0, 0, 16, 17, 1,
+                      0, 16, 0, 17, 1, 0, 17, 0, 17, 1, 16, 17, 0, 16, 1]
 
     def test_pixel_array_to_divoom(self):
-        divoom_data = divoom_pixel_array.pixel_array_to_divoom(self.TEST_BITS)
-        self.assertEquals(divoom_data, self.TEST_DIVOOM)
+        encoded_pixel_array = pixel_array_to_divoom(self.PIXEL_ARRAY)
+        self.assertEqual(encoded_pixel_array, self.DIVOOM_ENCODED)
+
+    def text_divoom_to_pixel_array(self):
+        decoded_divoom_data = divoom_to_pixel_array(self.DIVOOM_ENCODED)
+        self.assertEqual(decoded_divoom_data, self.PIXEL_ARRAY)
