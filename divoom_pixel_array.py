001e4b25c9c5e8adb248c1f20c3474b051c9da3b
Brett Crowell <brettcrowell@gmail.com>
2018-02-21 20:37:20 -0500

    use divoom palette natively instead of 16bit bmp

--- divoom_pixel_array.py
+++ divoom_pixel_array.py
@@ -1,4 +1,18 @@
-from divoom_image import to_
+# divoom palette
+# 0 = black
+# 1 = red
+# 2 = green
+# 3 = yellow
+# 4 = blue
+# 5 = pink
+# 6 = light blue
+# 7 = white
+
+def to_(a, b):
+    '''Join every 2 (4 bit) pixels into one byte'''
+    upper = b << 4
+    lower = a
+    return upper + lower
 
 def pixel_array_to_divoom(pixels):
     '''Converts an array of 100 integers, each representing a pixel, to Divoom data'''
@@ -8,4 +22,5 @@ def pixel_array_to_divoom(pixels):
         val = to_(i, next(it))
         result.append(val)
 
+    return result
     return result
