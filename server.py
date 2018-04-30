9bae87024fd20cf83f8dd19a6dbb20098e4b39c6
Brett Crowell <brettcrowell@gmail.com>
2018-04-29 23:34:52 -0400

    allow multiple command instances to be stored

--- server.py
+++ server.py
@@ -1,16 +1,17 @@
 # base httpserver example from...
 # https://gist.github.com/bradmontgomery/2219997
 
-from sys import argv
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from example_commands import ExampleCommands
 import json
 
 class S(BaseHTTPRequestHandler):
 
-    def _set_divoom(self):
-        if not hasattr(self, "commands"):
-            self.commands = ExampleCommands(argv[1])
+    devices = {}
+
+    def _set_divoom(self, bluetooth_address):
+        if not hasattr(self.devices, bluetooth_address):
+            self.devices[bluetooth_address] = ExampleCommands(bluetooth_address)
 
     def do_OPTIONS(self):
         self.send_response(200, "ok")
@@ -36,16 +37,18 @@ class S(BaseHTTPRequestHandler):
     def do_POST(self):
         self._set_headers()
         self.wfile.write("posted".encode("utf-8"))
-        self._set_divoom()
 
         length = int(self.headers["content-length"])
         payload = self.rfile.read(length)
         data = json.loads(payload.decode("utf-8"))
 
+        bluetooth_address = data["device"]
         action = data["action"]
         value = data["value"]
 
-        with self.commands as commands:
+        self._set_divoom(bluetooth_address)
+
+        with self.devices[bluetooth_address] as commands:
             if action == "show_file":
                 commands.show_file(value)
             elif action == "draw_text":
